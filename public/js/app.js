b4dad45a9386c60b5d3362816d858ef8125d2ac6
zhangtiantian10 <975571814@qq.com>
2016-08-07 12:01:15 +0800

    add bootstrap

--- public/js/app.js
+++ public/js/app.js
@@ -41,10 +41,16 @@ class App extends React.Component {
 
 class Editor extends React.Component {
     render() {
-        return <div>
-            <ReactRouter.Link to="/Preview"><button>Preview</button></ReactRouter.Link>
-            <RightButton onAdd={this.props.onAdd}/>
+        return <div className="row">
+            <div className="center">
+            <ReactRouter.Link to="/Preview">
+                <button type="button" className="btn btn-primary btn-lg">Preview</button>
+                <br/><br/>
+            </ReactRouter.Link>
+                <br/>
+            </div>
             <LeftPanel elements={this.props.elements} onDelete={this.props.onDelete}/>
+            <RightButton onAdd={this.props.onAdd}/>
         </div>;
     }
 }
@@ -55,9 +61,13 @@ class RightButton extends React.Component {
     }
 
     render() {
-        return <div>
-            <button onClick={this.add.bind(this, 'text')}>Text</button>
-            <button onClick={this.add.bind(this, 'date')}>Date</button>
+        return <div className="col-md-4">
+            <div className="center">
+                <button type="button" className="btn btn-link btn-lg" onClick={this.add.bind(this, 'text')}>Text</button>
+            </div>
+            <div className="center">
+                <button type="button" className="btn btn-link btn-lg" onClick={this.add.bind(this, 'date')}>Date</button>
+            </div>
         </div>;
     }
 }
@@ -69,13 +79,15 @@ class LeftPanel extends React.Component {
 
     render() {
         const elements = this.props.elements.map((ele, index) => {
-            return <div key={index}>
-                <input type={ele}/>
-                <button onClick={this.deleteForm.bind(this, index)}>X</button>
+            return <div key={index} className=" center">
+                <input type={ele} className="form-control inline"/>
+                <button type="button" className="btn btn-link btn-lg inline" onClick={this.deleteForm.bind(this, index)}>
+                    <span className="glyphicon glyphicon-remove-circle" aria-hidden="true"></span>
+                </button>
             </div>;
         })
 
-        return <div>
+        return <div className="col-md-8 groove">
             {elements}
         </div>
     }
@@ -84,15 +96,28 @@ class LeftPanel extends React.Component {
 class Preview extends React.Component {
     render() {
         const elements = this.props.elements.map((ele, index) => {
-            return <div key={index}>
-                <input type={ele}/>
+            return <div key={index} className="center">
+                <input type={ele} className="form-control inline"/>
             </div>;
         })
 
         return <div>
-            <ReactRouter.Link to="/"><button>Editor</button></ReactRouter.Link>
-            {elements}
-            <button>Sumbit</button>
+            <div className="center">
+                <ReactRouter.Link to="/">
+                    <button type="button" className="btn btn-primary btn-lg">Edit</button>
+                </ReactRouter.Link>
+            </div>
+            <br/>
+            <div className="groove">
+                <br/>
+                <form role="form">
+                    {elements}
+                </form>
+            </div>
+            <div className="center">
+                <br/>
+                <button type="button" className="btn btn-primary btn-lg">Sumbit</button>
+            </div>
         </div>
     }
 }
