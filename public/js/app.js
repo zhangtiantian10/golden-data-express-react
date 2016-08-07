23eb34140ff1d646f43acddbe52f95fde0d83dd9
zhangtiantian10 <975571814@qq.com>
2016-08-07 11:17:52 +0800

    add AJAX post request

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,27 @@
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            elements: []
+        }
+    }
+
+    componentDidMount() {
+        $.get('/api/forms/1', (elements) => {
+            this.setState({elements});
+        });
+    }
+
+    sumbit() {
+        $.post('/api/forms', {elements: ['world']},( data ) => {
+            this.setState({elements: data});
+        }, "json");
+    }
+
     render() {
         return <div>
-            Hello
+            {this.state.elements}
+            <button onClick={this.sumbit.bind(this)}>提交</button>
         </div>
     }
 }
--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = new express();
 const path = require('path');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded());
 
 app.use(express.static(__dirname + '/public'));
+
+app.get('/api/forms/:id', (req, res) => {
+    res.json(['hello']);
+});
+
+app.post('/api/forms', (req, res) => {
+    console.log(req.body.elements);
+    res.json(req.body.elements);
+});
 
 app.listen(3000, () => {
     console.log('Server started.');
