c632139d544580d71339d49633ab1ef291f3f7fa
zhangtiantian10 <975571814@qq.com>
2016-08-07 11:48:55 +0800

    data save to data.json

--- server.js
+++ server.js
@@ -1,29 +1,72 @@
 const express = require('express');
 const app = new express();
 const path = require('path');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded());
 
 app.use(express.static(__dirname + '/public'));
 
-let elements = [];
-
 app.get('/api/forms/:id', (req, res) => {
-    res.json(elements);
+    fs.readFile('./data.json', 'utf8', function (err, data) {
+        if(err){
+            next(err);
+            return;
+        }
+
+        if (data === '') {
+            res.status(200).json([]);
+
+        }
+        else {
+            var newData = JSON.parse(data);
+
+            res.status(200).json(newData);
+        }
+    });
 });
 
 app.post('/api/forms', (req, res) => {
-    elements.push(req.body.type);
 
-    res.json(elements);
-});
+    fs.readFile('./data.json', 'utf8', function (err, data) {
+        if(err){
+            return;
+        }
+        let newData = [];
 
-app.delete('/api/forms', (req, res) => {
-    elements.splice(req.body.index, 1);
+        if (data != '') {
+            newData = JSON.parse(data);
+        }
+        newData.push(req.body.type);
 
-    res.json(elements);
+        fs.writeFile('./data.json', JSON.stringify(newData), function (err) {
+
+            return;
+        });
+        res.status(200).json(newData);
+    });
 })
+
+app.delete('/api/forms', (req,res) => {
+    fs.readFile('./data.json', 'utf8', function (err, data) {
+        if(err){
+            return;
+        }
+        let newData = [];
+
+        if (data != '') {
+            newData = JSON.parse(data);
+        }
+        newData.splice(req.body.index, 1);
+
+        fs.writeFile('./data.json', JSON.stringify(newData), function (err) {
+
+            return;
+        });
+        res.status(200).json(newData);
+    });
+});
 
 app.listen(3000, () => {
     console.log('Server started.');
